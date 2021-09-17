67db8d554749f1c40f6634fc65f1eb1628189301
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2021-09-17 11:46:10 +0800

    wip

---
+++ securityGroups.tf
@@ -0,0 +1,86 @@
+resource "aws_security_group" "lb" {
+  name        = "jiehao-starter-kit-lb"
+  description = "Allow inbound traffic only from cloudflare IPs"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress = [
+    # allow all cloudflare IPs
+  ]
+
+  egress = [
+    {
+      description      = "Allow outbound HTTP traffic"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    },
+    {
+      description      = "Allow outbound HTTP traffic"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  tags = {
+    Name = "starter-kit-lb"
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "jiehao-starter-kit-ec2"
+  description = "Allow inbound traffic only from load balancer"
+  vpc_id      = module.vpc.vpc_id
+  ingress = [
+    {
+      description      = "Allow traffic from load balancer"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      security_groups  = [aws_security_group.lb.id]
+      cidr_blocks      = null
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+      self             = null
+    },
+    {
+      description      = "Allow traffic from load balancer"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      security_groups = [aws_security_group.lb.id]
+      cidr_blocks      = null
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+      self             = null
+    }
+  ]
+
+  egress = [
+    {
+      description      = "Allow all outbound traffic"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  tags = {
+    Name = "starter-kit-ec2"
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,33 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jiehao-test"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway  = true
+  reuse_nat_ips       = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "jiehao-test"
+  }
+}
+
+# to-do: add dns query logging
+resource "aws_flow_log" "example" {
+  log_destination      = aws_s3_bucket.test_app_flow_logs.arn
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = module.vpc.vpc_id
+}
+
+resource "aws_s3_bucket" "test_app_flow_logs" {
+  bucket = "test-app-flow-logs"
+  force_destroy = true
+}
