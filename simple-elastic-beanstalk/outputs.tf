8ad908c2ba44183b7f795653f88aaeb3f2f48c6c
Yuanruo Liang <yuanruo@open.gov.sg>
2022-01-03 22:51:45 +0800

    feat: output useful connection info

--- simple-elastic-beanstalk/outputs.tf
+++ simple-elastic-beanstalk/outputs.tf
@@ -1,39 +1,21 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
+output "eb_endpoint" {
+  value = module.elastic_beanstalk_environment.endpoint
 }
 
-output "vpc_cidr_block" {
-  description = "VPC CIDR block"
-  value       = module.vpc.vpc_cidr_block
+output "db_endpoint" {
+  value = module.db.db_instance_endpoint
+}
+output "db_port" {
+  value = module.db.db_instance_port
 }
 
-output "azs" {
-  value = module.vpc.azs
+output "db_schema_name" {
+  value = module.db.db_instance_name
 }
 
-output "public_subnets" {
-  description = "Public subnet IDs"
-  value       = module.vpc.public_subnets
+output "db_master_username" {
+  value = module.db.db_instance_username
 }
-output "public_subnets_cidr_blocks" {
-  value = module.vpc.public_subnets_cidr_blocks
-}
-
-output "private_subnets" {
-  description = "Private subnet IDs"
-  value       = module.vpc.private_subnets
-}
-
-output "private_subnets_cidr_blocks" {
-  value = module.vpc.private_subnets_cidr_blocks
-}
-
-output "database_subnets" {
-  description = "Database subnet IDs"
-  value       = module.vpc.database_subnets
-}
-
-output "database_subnets_cidr_blocks" {
-  value = module.vpc.database_subnets_cidr_blocks
+output "db_master_password" {
+  value = module.db.db_master_password
 }
